74cb81e777ce5b9dd4f9943bdad414962b28a6f6
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2020-11-30 19:54:15 +0100

    2016-01

--- src/bin/2016/01.rs
+++ src/bin/2016/01.rs
@@ -1,5 +1,5 @@
-use std::time::Instant;
 use std::collections::HashSet;
+use std::time::Instant;
 
 use common::io;
 
@@ -7,47 +7,43 @@ fn read_input() -> String {
   return io::read_input("2016-01");
 }
 
-fn parse_input(input: &str) -> Vec<(char,i32)> {
-  input.split(",")
+fn parse_input(input: &str) -> Vec<(char, i32)> {
+  input
+    .split(",")
     .map(|s| s.trim())
-    .map(|s| {
-    (s.chars().nth(0).unwrap(), s[1..].parse::<i32>().unwrap())
-  }).collect()
+    .map(|s| (s.chars().nth(0).unwrap(), s[1..].parse::<i32>().unwrap()))
+    .collect()
 }
 
 #[allow(unused_variables)]
 fn part_one(input: &str) -> i32 {
   let mut facing = 'N';
-  let mut pos:(i32,i32) = (0,0);
-  parse_input(input).iter().for_each(|(turn,steps)| {
+  let mut pos: (i32, i32) = (0, 0);
+  parse_input(input).iter().for_each(|(turn, steps)| {
     facing = match turn {
-      'R' => {
-        match facing {
-          'N' => 'E',
-          'S' => 'W',
-          'E' => 'S',
-          'W' => 'N',
-          _ => panic!("impossiburu")
-        }
+      'R' => match facing {
+        'N' => 'E',
+        'S' => 'W',
+        'E' => 'S',
+        'W' => 'N',
+        _ => panic!("impossiburu"),
       },
-      'L' => {
-        match facing {
-          'N' => 'W',
-          'S' => 'E',
-          'E' => 'N',
-          'W' => 'S',
-          _ => panic!("impossiburu")
-        }
+      'L' => match facing {
+        'N' => 'W',
+        'S' => 'E',
+        'E' => 'N',
+        'W' => 'S',
+        _ => panic!("impossiburu"),
       },
-      _ => panic!("impossiburu")
+      _ => panic!("impossiburu"),
     };
 
     match facing {
       'N' => pos = (pos.0, pos.1 + steps),
       'E' => pos = (pos.0 + steps, pos.1),
-      'S' => pos = (pos.0, pos.1 -steps),
-      'W' => pos = (pos.0 -steps, pos.1),
-      _ => panic!("impossiburu")
+      'S' => pos = (pos.0, pos.1 - steps),
+      'W' => pos = (pos.0 - steps, pos.1),
+      _ => panic!("impossiburu"),
     };
   });
   return pos.0.abs() + pos.1.abs();
@@ -56,39 +52,35 @@ fn part_one(input: &str) -> i32 {
 #[allow(unused_variables)]
 fn part_two(input: &str) -> i32 {
   let mut facing = 'N';
-  let mut pos:(i32,i32) = (0,0);
-  let mut visited:HashSet<(i32,i32)> = HashSet::new();
+  let mut pos: (i32, i32) = (0, 0);
+  let mut visited: HashSet<(i32, i32)> = HashSet::new();
   visited.insert(pos);
-  for (turn,steps) in parse_input(input).iter() {
+  for (turn, steps) in parse_input(input).iter() {
     facing = match turn {
-      'R' => {
-        match facing {
-          'N' => 'E',
-          'S' => 'W',
-          'E' => 'S',
-          'W' => 'N',
-          _ => panic!("impossiburu")
-        }
+      'R' => match facing {
+        'N' => 'E',
+        'S' => 'W',
+        'E' => 'S',
+        'W' => 'N',
+        _ => panic!("impossiburu"),
       },
-      'L' => {
-        match facing {
-          'N' => 'W',
-          'S' => 'E',
-          'E' => 'N',
-          'W' => 'S',
-          _ => panic!("impossiburu")
-        }
+      'L' => match facing {
+        'N' => 'W',
+        'S' => 'E',
+        'E' => 'N',
+        'W' => 'S',
+        _ => panic!("impossiburu"),
       },
-      _ => panic!("impossiburu")
+      _ => panic!("impossiburu"),
     };
 
-    for i in 1..steps+1 {
+    for i in 1..steps + 1 {
       match facing {
         'N' => pos = (pos.0, pos.1 + 1),
         'E' => pos = (pos.0 + 1, pos.1),
-        'S' => pos = (pos.0, pos.1 -1),
-        'W' => pos = (pos.0 -1, pos.1),
-        _ => panic!("impossiburu")
+        'S' => pos = (pos.0, pos.1 - 1),
+        'W' => pos = (pos.0 - 1, pos.1),
+        _ => panic!("impossiburu"),
       };
       if visited.contains(&pos) {
         return pos.0.abs() + pos.1.abs();
