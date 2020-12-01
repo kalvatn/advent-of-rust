71cdc01dd993c499101c4e836340028ecc91bc57
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2020-12-01 06:25:41 +0100

    2020-01

--- src/bin/2020/01.rs
+++ src/bin/2020/01.rs
@@ -6,23 +6,41 @@ fn read_input() -> String {
   return io::read_input("2020-01");
 }
 
-fn parse_input(input: &str) -> Vec<char> {
-  return input.chars().collect();
+fn parse_input(input: &str) -> Vec<u32> {
+  input
+    .lines()
+    .map(|line| line.parse::<u32>().unwrap())
+    .collect()
 }
 
-#[allow(unused_variables)]
-fn part_one(input: &str) -> usize {
-  return 0;
+fn part_one(input: &str) -> u32 {
+  let numbers = parse_input(input);
+  for i in &numbers {
+    for j in &numbers {
+      if j != i && j + i == 2020 {
+        return j * i;
+      }
+    }
+  }
+  panic!("impossiburu");
 }
 
-#[allow(unused_variables)]
-fn part_two(input: &str) -> usize {
-  return 0;
+fn part_two(input: &str) -> u32 {
+  let numbers = parse_input(input);
+  for i in &numbers {
+    for j in &numbers {
+      for k in &numbers {
+        if j != i && j != k && j + i + k == 2020 {
+          return j * i * k;
+        }
+      }
+    }
+  }
+  panic!("impossiburu");
 }
 
 fn main() {
   let input = read_input();
-  println!("{}", input);
 
   let p1_timer = Instant::now();
   println!(
@@ -45,11 +63,11 @@ mod test {
 
   #[test]
   fn test_part_one() {
-    assert_eq!(part_one(&read_input()), 0);
+    assert_eq!(part_one(&read_input()), 32064);
   }
 
   #[test]
   fn test_part_two() {
-    assert_eq!(part_two(&read_input()), 0);
+    assert_eq!(part_two(&read_input()), 193598720);
   }
 }
