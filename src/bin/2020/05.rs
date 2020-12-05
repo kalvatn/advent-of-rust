8972218c7c6e71e374b4c1b16e9564fd846b879a
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2020-12-05 08:08:46 +0100

    2020-05 cleanup

--- src/bin/2020/05.rs
+++ src/bin/2020/05.rs
@@ -1,13 +1,4 @@
-#![allow(unused_variables, unused_imports)]
-
-use std::collections::{HashMap, HashSet};
-use std::thread::sleep_ms;
 use std::time::Instant;
-
-use itertools::Itertools;
-use lazy_static::lazy_static;
-use regex::Regex;
-use serde_scan::scan;
 
 use common::io;
 
@@ -15,49 +6,53 @@ fn read_input() -> String {
   return io::read_input("2020-05");
 }
 
-fn parse_input(input: &str) -> &str {
-  for line in input.lines() {
-    // let line = "#1 @ 555,891: 18x12";
-    // let parsed = scan!("#{} @ {},{}: {}x{}" <- line)?;
-  }
-  return input;
-}
-
-fn part_one(input: &str) -> usize {
-  let mut best = 0;
-  for line in input.lines() {
-    let row = get_row(line);
-    let seat = get_seat(line);
-    let lol = row * 8 + seat;
-
-    if lol >= best {
-      best = lol;
+fn get_seat(encoded: &str) -> (u16, u16) {
+  let mut row: (u16, u16) = (0, 128);
+  let mut col: (u16, u16) = (0, 8);
+  for (i, c) in encoded.char_indices() {
+    if i < 7 {
+      match c {
+        'F' => row.1 -= (row.1 - row.0) / 2,
+        'B' => row.0 += (row.1 - row.0) / 2,
+        _ => unreachable!("invalid"),
+      }
+    } else {
+      match c {
+        'L' => col.1 -= (col.1 - col.0) / 2,
+        'R' => col.0 += (col.1 - col.0) / 2,
+        _ => unreachable!("invalid"),
+      }
     }
   }
-  return best as usize;
+  return (row.0, col.0);
 }
 
-fn part_two(input: &str) -> usize {
-  let mut best = 0;
-  let mut seat_ids = vec![];
-  for line in input.lines() {
-    let row = get_row(line);
-    if row > 0 && row < 128 {
-      let col = get_seat(line);
-      let seat_id = row * 8 + col;
-      seat_ids.push(seat_id);
-    }
-  }
+fn get_seat_ids(input: &str) -> Vec<u16> {
+  input
+    .lines()
+    .map(|l| {
+      let (row, col) = get_seat(l);
+      row * 8 + col
+    })
+    .collect()
+}
+
+fn part_one(input: &str) -> u16 {
+  *get_seat_ids(input).iter().max().unwrap()
+}
+
+fn part_two(input: &str) -> u16 {
+  let mut seat_ids = get_seat_ids(input);
   seat_ids.sort();
   let mut prev = seat_ids[0];
   for i in 1..seat_ids.len() {
     let x = seat_ids[i];
     if (x - prev) > 1 {
-      return (prev as usize) + 1;
+      return prev + 1;
     }
     prev = x;
   }
-  return best as usize;
+  unreachable!("impossiburu")
 }
 
 fn main() {
@@ -74,59 +69,24 @@ fn main() {
   println!("part two {:?} {:?}", p2, p2_time);
 }
 
-fn get_seat(p0: &str) -> i32 {
-  let mut low = 0;
-  let mut high = 8;
-  let mut i = 7;
-  while i < p0.len() {
-    let c = p0.chars().nth(i).unwrap();
-    match c {
-      'L' => {
-        high -= (high - low) / 2;
-      }
-      'R' => low += (high - low) / 2,
-      _ => unreachable!("invalid"),
-    }
-    println!("{} {} {} {}", i, c, low, high);
-    i += 1;
-  }
-  return low;
-}
-
-fn get_row(p0: &str) -> i32 {
-  let mut low = 0;
-  let mut high = 128;
-  let mut i = 0;
-  while i < 7 {
-    let c = p0.chars().nth(i).unwrap();
-    match c {
-      'F' => high -= (high - low) / 2,
-      'B' => low += (high - low) / 2,
-      _ => unreachable!("invalid"),
-    }
-    i += 1;
-  }
-  return low;
-}
-
 #[cfg(test)]
 mod test {
   use super::*;
 
   #[test]
   fn test_get_row() {
-    assert_eq!(get_row("FBFBBFFRLR"), 44);
-    assert_eq!(get_row("BFFFBBFRRR"), 70);
-    assert_eq!(get_row("FFFBBBFRRR"), 14);
-    assert_eq!(get_row("BBFFBBFRLL"), 102);
+    assert_eq!(get_seat("FBFBBFFRLR").0, 44);
+    assert_eq!(get_seat("BFFFBBFRRR").0, 70);
+    assert_eq!(get_seat("FFFBBBFRRR").0, 14);
+    assert_eq!(get_seat("BBFFBBFRLL").0, 102);
   }
 
   #[test]
-  fn test_get_seat() {
-    assert_eq!(get_seat("FBFBBFFRLR"), 5);
-    assert_eq!(get_seat("BFFFBBFRRR"), 7);
-    assert_eq!(get_seat("FFFBBBFRRR"), 7);
-    assert_eq!(get_seat("BBFFBBFRLL"), 4);
+  fn test_get_col() {
+    assert_eq!(get_seat("FBFBBFFRLR").1, 5);
+    assert_eq!(get_seat("BFFFBBFRRR").1, 7);
+    assert_eq!(get_seat("FFFBBBFRRR").1, 7);
+    assert_eq!(get_seat("BBFFBBFRLL").1, 4);
   }
 
   #[test]
