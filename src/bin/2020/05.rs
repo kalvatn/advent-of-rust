2262e05bfc95525462fa06e1fa2c66509a0fe8fb
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2020-12-05 06:59:01 +0100

    2020-05

--- src/bin/2020/05.rs
+++ src/bin/2020/05.rs
@@ -1,10 +1,13 @@
 #![allow(unused_variables, unused_imports)]
 
+use std::collections::{HashMap, HashSet};
+use std::thread::sleep_ms;
+use std::time::Instant;
+
 use itertools::Itertools;
 use lazy_static::lazy_static;
 use regex::Regex;
-use std::collections::{HashMap, HashSet};
-use std::time::Instant;
+use serde_scan::scan;
 
 use common::io;
 
@@ -13,20 +16,52 @@ fn read_input() -> String {
 }
 
 fn parse_input(input: &str) -> &str {
+  for line in input.lines() {
+    // let line = "#1 @ 555,891: 18x12";
+    // let parsed = scan!("#{} @ {},{}: {}x{}" <- line)?;
+  }
   return input;
 }
 
 fn part_one(input: &str) -> usize {
-  return 0;
+  let mut best = 0;
+  for line in input.lines() {
+    let row = get_row(line);
+    let seat = get_seat(line);
+    let lol = row * 8 + seat;
+
+    if lol >= best {
+      best = lol;
+    }
+  }
+  return best as usize;
 }
 
 fn part_two(input: &str) -> usize {
-  return 0;
+  let mut best = 0;
+  let mut seat_ids = vec![];
+  for line in input.lines() {
+    let row = get_row(line);
+    if row > 0 && row < 128 {
+      let col = get_seat(line);
+      let seat_id = row * 8 + col;
+      seat_ids.push(seat_id);
+    }
+  }
+  seat_ids.sort();
+  let mut prev = seat_ids[0];
+  for i in 1..seat_ids.len() {
+    let x = seat_ids[i];
+    if (x - prev) > 1 {
+      return (prev as usize) + 1;
+    }
+    prev = x;
+  }
+  return best as usize;
 }
 
 fn main() {
   let input = read_input();
-  println!("{:?}", input);
 
   let time = Instant::now();
   let p1 = part_one(&input);
@@ -39,17 +74,68 @@ fn main() {
   println!("part two {:?} {:?}", p2, p2_time);
 }
 
+fn get_seat(p0: &str) -> i32 {
+  let mut low = 0;
+  let mut high = 8;
+  let mut i = 7;
+  while i < p0.len() {
+    let c = p0.chars().nth(i).unwrap();
+    match c {
+      'L' => {
+        high -= (high - low) / 2;
+      }
+      'R' => low += (high - low) / 2,
+      _ => unreachable!("invalid"),
+    }
+    println!("{} {} {} {}", i, c, low, high);
+    i += 1;
+  }
+  return low;
+}
+
+fn get_row(p0: &str) -> i32 {
+  let mut low = 0;
+  let mut high = 128;
+  let mut i = 0;
+  while i < 7 {
+    let c = p0.chars().nth(i).unwrap();
+    match c {
+      'F' => high -= (high - low) / 2,
+      'B' => low += (high - low) / 2,
+      _ => unreachable!("invalid"),
+    }
+    i += 1;
+  }
+  return low;
+}
+
 #[cfg(test)]
 mod test {
   use super::*;
 
   #[test]
+  fn test_get_row() {
+    assert_eq!(get_row("FBFBBFFRLR"), 44);
+    assert_eq!(get_row("BFFFBBFRRR"), 70);
+    assert_eq!(get_row("FFFBBBFRRR"), 14);
+    assert_eq!(get_row("BBFFBBFRLL"), 102);
+  }
+
+  #[test]
+  fn test_get_seat() {
+    assert_eq!(get_seat("FBFBBFFRLR"), 5);
+    assert_eq!(get_seat("BFFFBBFRRR"), 7);
+    assert_eq!(get_seat("FFFBBBFRRR"), 7);
+    assert_eq!(get_seat("BBFFBBFRLL"), 4);
+  }
+
+  #[test]
   fn test_part_one() {
-    assert_eq!(part_one(&read_input()), 0);
+    assert_eq!(part_one(&read_input()), 835);
   }
 
   #[test]
   fn test_part_two() {
-    assert_eq!(part_two(&read_input()), 0);
+    assert_eq!(part_two(&read_input()), 649);
   }
 }
