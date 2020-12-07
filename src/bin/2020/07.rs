a27744eb082a65e77ce9aad353d5d0ccf5e684a3
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2020-12-07 22:29:54 +0100

    2020-07 cleanup WIP

--- src/bin/2020/07.rs
+++ src/bin/2020/07.rs
@@ -1,14 +1,12 @@
-#![allow(unused_variables, unused_imports)]
-
-use itertools::Itertools;
 use lazy_static::lazy_static;
 use regex::Regex;
 use std::collections::{HashMap, HashSet, VecDeque};
 use std::time::Instant;
 
 use common::io;
-use std::borrow::{Borrow, BorrowMut};
-use std::collections::hash_map::RandomState;
+// use std::borrow::{BorrowMut};
+use std::rc::Rc;
+use std::cell::RefCell;
 
 #[derive(Debug)]
 struct Bag {
@@ -21,66 +19,71 @@ fn read_input() -> String {
   return io::read_input("2020-07");
 }
 
-fn parse_input(input: &str) -> HashMap<String, Vec<(u32, String)>> {
+fn parse_input(input: &str) -> HashMap<String, Bag> {
   lazy_static! {
     static ref RE_HAS_BAGS: Regex = Regex::new(r"^([\w+\s]+) bags contain").unwrap();
     static ref RE_YES_BAGS: Regex = Regex::new(r"(\d+) ([\w+\s]+) bags?").unwrap();
   }
-  let mut map: HashMap<String, Vec<(u32, String)>> = HashMap::new();
+  let bag_map= Rc::new(RefCell::new(HashMap::<String,Bag>::new()));
   for line in input.lines() {
     if RE_HAS_BAGS.is_match(line) {
       RE_HAS_BAGS.captures(line).map(|cap| {
         let name: String = cap[1].to_string();
-        let contains: Vec<(u32, String)> = vec![];
-        let vec = map.entry(name.clone()).or_insert(contains);
         let split = line.split(",").collect::<Vec<&str>>();
-        split.iter().for_each(|l| {
+        let parent = bag_map.borrow_mut().entry(name.to_string()).or_insert( Bag {
+          name : name.to_string(),
+          contains: HashMap::new(),
+          contained_by: vec![]
+        });
+        split.iter().map(|l| {
           RE_YES_BAGS.captures(l.trim()).map(|cap| {
             let count = cap[1].parse::<u32>().unwrap();
             let other_name: String = cap[2].to_string();
-            vec.push((count, other_name.clone()));
-          });
+            parent.contains.insert(other_name.to_string(), count);
+            bag_map.clone().borrow_mut().entry(other_name.to_string()).or_insert(
+            Bag {
+              name : other_name.to_string(),
+              contains: HashMap::new(),
+              contained_by: vec![]
+            }).contained_by.push(name.to_string());
+          }).unwrap();
         });
       });
     }
   }
-  return map;
+  // for (k, v) in &map {
+  //   let mut map1 = HashMap::new();
+  //   for (count, other) in v {
+  //     map1.insert(other.to_owned(), count.to_owned());
+  //   }
+  //   let bag = Bag {
+  //     name: k.to_string(),
+  //     contains: map1.to_owned(),
+  //     contained_by: vec![],
+  //   };
+  //   bag_map.insert(k.to_string(), bag);
+  // }
+  // let mut contained_by: HashMap<String, HashSet<String>> = HashMap::new();
+  // for x in bag_map.values() {
+  //   for c in x.contains.keys() {
+  //     contained_by
+  //       .entry(c.to_string())
+  //       .or_insert(HashSet::new())
+  //       .insert(x.name.to_string());
+  //   }
+  // }
+  // for (k, v) in bag_map.borrow_mut() {
+  //   if contained_by.contains_key(k) {
+  //     for x in contained_by.get(k).unwrap() {
+  //       v.contained_by.push(x.to_string());
+  //     }
+  //   }
+  // }
+  return bag_map.into_inner();
 }
 
 fn part_one(input: &str) -> usize {
-  let input_map = parse_input(input);
-  let mut bag_map: HashMap<String, Bag> = HashMap::new();
-  for (k, v) in &input_map {
-    println!("{} contains {:?}", k, v);
-    let mut map1 = HashMap::new();
-    for (count, other) in v {
-      map1.insert(other.to_owned(), count.to_owned());
-    }
-    let bag = Bag {
-      name: k.to_string(),
-      contains: map1.to_owned(),
-      contained_by: vec![],
-    };
-    bag_map.insert(k.to_string(), bag);
-  }
-  let mut contained_by: HashMap<String, HashSet<String>> = HashMap::new();
-  for x in bag_map.values() {
-    for c in x.contains.keys() {
-      contained_by
-        .entry(c.to_string())
-        .or_insert(HashSet::new())
-        .insert(x.name.to_string());
-    }
-  }
-  for (k, mut v) in bag_map.borrow_mut() {
-    if contained_by.contains_key(k) {
-      for x in contained_by.get(k).unwrap() {
-        v.contained_by.push(x.to_string());
-      }
-    }
-    println!("{} {:?}", k, v);
-  }
-
+  let bag_map = parse_input(input);
   let mut colors = HashSet::<String>::new();
   let mut queue = VecDeque::<&Bag>::new();
   queue.push_front(bag_map.get("shiny gold").unwrap());
@@ -95,48 +98,15 @@ fn part_one(input: &str) -> usize {
 }
 
 fn part_two(input: &str) -> usize {
-  let input_map = parse_input(input);
-  let mut bag_map: HashMap<String, Bag> = HashMap::new();
-  for (k, v) in &input_map {
-    println!("{} contains {:?}", k, v);
-    let mut map1 = HashMap::new();
-    for (count, other) in v {
-      map1.insert(other.to_owned(), count.to_owned());
-    }
-    let bag = Bag {
-      name: k.to_string(),
-      contains: map1.to_owned(),
-      contained_by: vec![],
-    };
-    bag_map.insert(k.to_string(), bag);
-  }
-  let mut contained_by: HashMap<String, HashSet<String>> = HashMap::new();
-  for x in bag_map.values() {
-    for c in x.contains.keys() {
-      contained_by
-        .entry(c.to_string())
-        .or_insert(HashSet::new())
-        .insert(x.name.to_string());
-    }
-  }
-  for (k, v) in bag_map.borrow_mut() {
-    if contained_by.contains_key(k) {
-      for x in contained_by.get(k).unwrap() {
-        v.contained_by.push(x.to_string());
-      }
-    }
-    println!("{} {:?}", k, v);
-  }
-
+  let bag_map: HashMap<String, Bag> = parse_input(input);
   let mut queue = VecDeque::<&Bag>::new();
   queue.push_front(bag_map.get("shiny gold").unwrap());
   let mut count = 0;
   while !queue.is_empty() {
     let bag = queue.pop_front().unwrap();
-    println!("{:?}", bag);
     for (k, v) in &bag.contains {
       count += v;
-      for i in 0..*v {
+      for _i in 0..*v {
         queue.push_back(bag_map.get(k).unwrap())
       }
     }
@@ -146,7 +116,6 @@ fn part_two(input: &str) -> usize {
 
 fn main() {
   let input = read_input();
-  println!("{:?}", input);
 
   let time = Instant::now();
   let p1 = part_one(&input);
