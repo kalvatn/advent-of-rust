fd37508482967ff67ede5a9f887801d2b3e1ecab
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2020-12-08 09:04:59 +0100

    2020-08 cleanup

--- src/bin/2020/08.rs
+++ src/bin/2020/08.rs
@@ -3,75 +3,93 @@ use std::time::Instant;
 
 use common::io;
 
+#[derive(PartialEq, Eq, Clone)]
+enum Op {
+  Nop,
+  Jmp,
+  Acc,
+}
+
+#[derive(Clone)]
+struct Instruction {
+  op: Op,
+  val: i16,
+}
+
 fn read_input() -> String {
   return io::read_input("2020-08");
 }
 
-fn parse_input(input: &str) -> Vec<(String, i32)> {
+fn parse_input(input: &str) -> Vec<Instruction> {
   input
     .lines()
     .map(|line| {
       let split = line.split(" ").collect::<Vec<&str>>();
-      (split[0].to_string(), split[1].parse::<i32>().unwrap())
+      Instruction {
+        op: match split[0] {
+          "jmp" => Op::Jmp,
+          "acc" => Op::Acc,
+          "nop" => Op::Nop,
+          _ => unreachable!("impossiburu"),
+        },
+        val: split[1].parse::<i16>().unwrap(),
+      }
     })
     .collect()
 }
 
-fn part_one(input: &str) -> i32 {
-  let instructions = parse_input(input);
-  return run_program(&instructions).0;
-}
-
-fn run_program(instructions: &Vec<(String, i32)>) -> (i32, bool) {
-  let mut acc = 0i32;
+fn run_program(instructions: &Vec<Instruction>, swap: Option<usize>) -> (i16, bool) {
+  let mut acc = 0i16;
   let mut i = 0usize;
   let mut seen: HashSet<usize> = HashSet::new();
-  while i < instructions.len() {
-    let (op, v) = &instructions[i as usize];
+  let len = instructions.len();
+  while i < len {
+    let instruction = &instructions[i];
     if seen.contains(&i) {
       return (acc, false);
     }
     seen.insert(i);
-    match op.as_str() {
-      "nop" => i += 1,
-      "acc" => {
-        acc += v;
+    let op = if swap.is_some() && swap.unwrap() == i {
+      match instruction.op {
+        Op::Nop => &Op::Jmp,
+        Op::Jmp => &Op::Nop,
+        _ => unreachable!(),
+      }
+    } else {
+      &instruction.op
+    };
+    match op {
+      Op::Nop => i += 1,
+      Op::Jmp => i = (i as i16 + instruction.val) as usize,
+      Op::Acc => {
+        acc += instruction.val;
         i += 1;
       }
-      "jmp" => i = (i as i32 + v) as usize,
-      _ => unreachable!("impossiburu"),
     }
   }
   (acc, true)
 }
 
-fn part_two(input: &str) -> i32 {
-  let original_input = parse_input(input);
-  let nop_jmp: &Vec<(usize, &(String, i32))> = &original_input
-    .iter()
-    .enumerate()
-    .filter(|(_i, (op, _v))| op == "jmp" || op == "nop")
-    .collect();
-  for replace in nop_jmp {
-    let replace_index = replace.0;
-    let (original_op, original_value) = &original_input[replace_index];
-    let new_op = match original_op.as_str() {
-      "jmp" => "nop",
-      "nop" => "jmp",
-      _ => unreachable!("not a 'jmp' or 'nop'"),
-    };
-    let mut instructions = original_input.clone();
-    instructions[replace.0] = (new_op.to_string(), *original_value);
-    let (acc, terminated_normally) = run_program(&instructions);
-    if terminated_normally {
-      return acc;
+fn part_one(instructions: &Vec<Instruction>) -> i16 {
+  return run_program(&instructions, None).0;
+}
+
+fn part_two(original_input: &Vec<Instruction>) -> i16 {
+  for (swap_index, ins) in original_input.iter().enumerate() {
+    if ins.op != Op::Acc {
+      let (acc, terminated_normally) = run_program(&original_input, Some(swap_index));
+      if terminated_normally {
+        return acc;
+      }
     }
   }
   unreachable!("fail")
 }
 
 fn main() {
-  let input = read_input();
+  let time = Instant::now();
+  let input = parse_input(&read_input());
+  let parse_time = time.elapsed();
 
   let time = Instant::now();
   let p1 = part_one(&input);
@@ -80,6 +98,7 @@ fn main() {
   let time = Instant::now();
   let p2 = part_two(&input);
   let p2_time = time.elapsed();
+  println!("parse {:?}", parse_time);
   println!("part one {:?} {:?}", p1, p1_time);
   println!("part two {:?} {:?}", p2, p2_time);
 }
@@ -100,13 +119,13 @@ acc +6";
 
   #[test]
   fn test_part_one() {
-    assert_eq!(part_one(TEST_INPUT), 5);
-    assert_eq!(part_one(&read_input()), 1317);
+    assert_eq!(part_one(&parse_input(TEST_INPUT)), 5);
+    assert_eq!(part_one(&parse_input(&read_input())), 1317);
   }
 
   #[test]
   fn test_part_two() {
-    assert_eq!(part_two(TEST_INPUT), 8);
-    assert_eq!(part_two(&read_input()), 1033);
+    assert_eq!(part_two(&parse_input(TEST_INPUT)), 8);
+    assert_eq!(part_two(&parse_input(&read_input())), 1033);
   }
 }
