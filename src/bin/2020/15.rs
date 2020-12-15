f6985b1174f161ea3e2166bcc28ae7e36f39b59b
Jon Terje Tvergrov Kalvatn <j.kalvatn@sportradar.com>
2020-12-15 06:46:00 +0100

    2020-15

--- src/bin/2020/15.rs
+++ src/bin/2020/15.rs
@@ -15,16 +15,59 @@ use std::time::Instant;
 
 use common::io;
 
+const TEST_INPUT: &str = "0,3,6\n";
+
 fn read_input() -> String {
   return io::read_input("2020-15");
 }
 
-fn parse_input(input: &str) -> &str {
-  return input;
+fn parse_input(input: &str) -> Vec<usize> {
+  return input
+    .lines()
+    .nth(0)
+    .unwrap()
+    .split(",")
+    .map(|c| c.parse::<usize>().unwrap())
+    .collect();
 }
 
 fn part_one(input: &str) -> usize {
-  return 0;
+  let mut last_spoken = HashMap::<usize, Vec<usize>>::new();
+  let numbers = parse_input(input);
+  let mut order = vec![];
+  for (i, n) in numbers.iter().enumerate() {
+    last_spoken.entry(*n).or_insert(vec![]).push(i + 1);
+    order.push(*n);
+  }
+  let mut i = numbers.len() + 1;
+  while i <= 30000000 {
+    let mut last = *order.last().unwrap();
+    if last_spoken.contains_key(&last) {
+      let indices = last_spoken.get(&last).unwrap();
+      let count = indices.len();
+      if count == 1 {
+        last = 0;
+        last_spoken.entry(last).or_insert(vec![]).push(i);
+      } else {
+        let (i1, i2) = (
+          indices.last().unwrap(),
+          indices.iter().nth(count - 2).unwrap(),
+        );
+        last = (i1 - i2);
+        last_spoken.entry(last).or_insert(vec![]).push(i);
+      }
+    } else {
+      last = 0;
+      last_spoken.entry(last).or_insert(vec![]).push(i);
+    }
+    i += 1;
+    order.push(last);
+  }
+  // for (k,v) in last_spoken {
+  //   println!("{} {:?}", k, v);
+  // }
+
+  return *order.last().unwrap();
 }
 
 fn part_two(input: &str) -> usize {
@@ -33,7 +76,6 @@ fn part_two(input: &str) -> usize {
 
 fn main() {
   let input = read_input();
-  println!("{:?}", input);
 
   let time = Instant::now();
   let p1 = part_one(&input);
